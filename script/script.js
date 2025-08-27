cefb1a2de43975e0dd65f12692845182461767b2
Teo <seminara.teodoro93@gmail.com>
2025-08-27 16:13:24 +0200

    esercizio completo

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ console.log("Km da percorrere:", kmRoad);
 
 // Valori fissi per calcolo biglietto
 
-const priceKm = kmRoad * 0.21;
+// const priceKm = kmRoad * 0.21;
 
 // console.log("Costo biglietto intero", priceKm);
 
@@ -21,28 +21,34 @@ const priceKm = kmRoad * 0.21;
 
 // if (userAge < 18) {
 
-//     console.log("Il costo del biglietto è:", ageTeen);
+//     console.log("Il costo del biglietto è:", ageTeen.toFixed(2));
 
 // } else if (userAge > 65) {
 
-//     console.log("Il costo del biglietto è:", ageOld);
+//     console.log("Il costo del biglietto è:", ageOld.toFixed(2));
 
 // } else {
 
-//     console.log("Il costo del biglietto è:", priceKm);
+//     console.log("Il costo del biglietto è:", priceKm.toFixed(2));
 
 // }
 
 
-let priceResult = priceKm
+let priceResult = kmRoad * 0.213
 
 if (userAge < 18) {
 
-    priceResult = priceKm * 0.80 ;
+    priceResult *= 0.80 ;
 
 } else if (userAge > 65) {
 
-    priceResult = priceKm * 0.60 ;
+    priceResult *= 0.60 ;
 }
 
-console.log("Devi pagare:", priceResult.toFixed(2));
+// console.log("Devi pagare:", priceResult.toFixed(2));
+
+// console.log(priceResult);
+
+
+console.log((Math.ceil(priceResult *100)/100).toFixed(2));
+
